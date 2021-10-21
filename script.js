cf0c068262e0aca5e8091e6ad40a0d7f0ee68285
Zhi Yong Han <hanzyong0@gmail.com>
2021-10-21 22:11:22 +0800

    add trailing effect to grid

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ function makeGrid(rows, cols) {
     for (let i = 0; i < (rows * cols); i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.textContent = 'hi';
         container.appendChild(square);
     }
 }
 
 makeGrid(16, 16);
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', (e) => {
+        e.target.classList.toggle('yellow');
+    });
+});

2cd94df6c420d8e3af126bcab0e91915b3e5d7d2
Zhi Yong Han <hanzyong0@gmail.com>
2021-10-23 16:00:13 +0800

    add feature to enable user to change grid size

--- script.js
+++ script.js
@@ -11,7 +11,50 @@ function makeGrid(rows, cols) {
     }
 }
 
-makeGrid(16, 16);
+// Reset button
+const btn = document.querySelector('#reset');
+
+function erase() {
+    location.reload();
+};
+
+// Prompt user for grid size
+let a;
+let b;
+function getRows() {
+    a = prompt('What is the number of rows?');
+    while (a > 100) {
+        alert('Number must be less than or equals to 100');
+        a = prompt('What is the number of rows?');
+    };
+    sessionStorage.setItem('a', String(a));
+};
+
+function getCols() {
+    b = prompt('What is the number of columns?');
+    while (b > 100) {
+        alert('Number must be less than or equals to 100');
+        b = prompt('What is the number of columns?');
+    };
+    sessionStorage.setItem('b', String(b));
+};
+
+// Reload and make new grid
+btn.addEventListener('click', () => {
+    erase();
+    getRows();
+    getCols();
+});
+
+a = Number(sessionStorage.getItem('a'));
+b = Number(sessionStorage.getItem('b'));
+
+// Check whether page is first load
+if (sessionStorage.getItem('a') == null || sessionStorage.getItem('b') == null) {
+    makeGrid(16, 16);
+} else {
+    makeGrid(a, b);
+};
 
 // Global variable to detect holding mousedown
 let isMouseDown = false;
@@ -39,11 +82,4 @@ squares.forEach((square) => {
     square.addEventListener('mousedown', (e) => {
         e.target.classList.add('yellow');
     });
-});
-
-// Reset button
-const btn = document.querySelector('#reset');
-
-btn.addEventListener('click', () => {
-    window.location.reload();
 });
